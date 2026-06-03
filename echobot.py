e98ed815305b521ade74e2ee77b2f8342671b2cf
XiaodanLeng <xiaodan.leng.neuro@gmail.com>
2026-06-03 14:16:34 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,4 +1,14 @@
-import streamlit as st
+# Slide 9
+#import streamlit as st
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
+#with st.chat_message("user"):
+#    st.write("Hello 👋")
+
+# Slide 12
+import streamlit as st
+import numpy as np
+
+with st.chat_message("assistant"):
+    st.write("Hello human")
+    st.bar_chart(np.random.randn(30, 3))
+
